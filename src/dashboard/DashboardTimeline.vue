87a69b77520df4b80f24f49f061d2e6374aac8ae
Luiz Vollrath <luiz.vollrath@outlook.com>
2019-07-12 14:55:14 -0300

    grafico no dark theme

--- src/dashboard/DashboardTimeline.vue
+++ src/dashboard/DashboardTimeline.vue
@@ -33,14 +33,16 @@ export default {
         //     name: 'Requisições por minuto'
         //   }
         // }),
+        colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+                 '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
         series: [{
-          data: this.seriesDataFila,
-          name: this.nameSerieFila || ''
-        },
-        {
-          data: this.seriesDataRequest,
-          name: this.nameSerieRequest || ''
-        }],
+            data: this.seriesDataFila,
+            name: this.nameSerieFila || ''
+          },
+          {
+            data: this.seriesDataRequest,
+            name: this.nameSerieRequest || ''
+          }],
         chart: {
           zoomType: 'x',
           backgroundColor: this.darkTheme ? '#222326' : '#ffffff'
@@ -52,10 +54,94 @@ export default {
             enabled: false
         },
         title: {
+            style: {
+              color: '#E0E0E3',
+              textTransform: 'uppercase',
+              fontSize: '20px'
+            },
             text: this.timelineTitle || '',
             align: 'left'
         },
+        subtitle: {
+          style: {
+            color: '#E0E0E3',
+            textTransform: 'uppercase'
+          }
+        },
+        yAxis: {
+          gridLineColor: '#707073',
+          labels: {
+              style: {
+                  color: '#E0E0E3'
+              }
+          },
+          lineColor: '#707073',
+          minorGridLineColor: '#505053',
+          tickColor: '#707073',
+          tickWidth: 1,
+          title: {
+              style: {
+                  color: '#A0A0A3'
+              }
+            }
+          },
+          tooltip: {
+              backgroundColor: 'rgba(0, 0, 0, 0.85)',
+              style: {
+                  color: '#F0F0F0'
+              }
+          },
+          plotOptions: {
+              series: {
+                  dataLabels: {
+                      color: '#B0B0B3'
+                  },
+                  marker: {
+                      lineColor: '#333'
+                  }
+              },
+              boxplot: {
+                  fillColor: '#505053'
+              },
+              candlestick: {
+                  lineColor: 'white'
+              },
+              errorbar: {
+                  color: 'white'
+              }
+          },
+          legend: {
+              itemStyle: {
+                  color: '#E0E0E3'
+              },
+              itemHoverStyle: {
+                  color: '#FFF'
+              },
+              itemHiddenStyle: {
+                  color: '#606063'
+              }
+          },    
+              labels: {
+        style: {
+            color: '#707073'
+        }
+    },
+    
         xAxis: {
+            gridLineColor: '#707073',
+            labels: {
+              style: {
+                  color: '#E0E0E3'
+              }
+            },
+            lineColor: '#707073',
+            minorGridLineColor: '#505053',
+            tickColor: '#707073',
+            title: {
+              style: {
+                color: '#A0A0A3'
+              }
+            },
             type: 'datetime'
 
         }
