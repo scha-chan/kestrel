55ce91291f871922a3acaf1b5d722767e975cb5f
Luiz Vollrath <luiz.vollrath@outlook.com>
2019-07-16 17:33:25 -0300

    Melhorado powershell para jogar no ambiente de aceite

--- replaceUnix.ps1
+++ replaceUnix.ps1
@@ -1,4 +1,4 @@
-$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist"-Recurse
+<#$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist" -file -Recurse 
 foreach ($file in $files){
 	$oldJS = '/js/'
 	$newJS = '/html/DashboardB2BG/js/'
@@ -7,7 +7,7 @@ foreach ($file in $files){
 	$content -replace $oldJS,$newJS | Out-File $($file.FullName) 
 }
 
-$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist"-Recurse
+$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist" -file -Recurse
 foreach ($file in $files){
 	$oldCSS = 'css/'
 	$newCSS = 'html/DashboardB2BG/css/'
@@ -16,7 +16,7 @@ foreach ($file in $files){
 	$content -replace $oldCSS,$newCSS | Out-File $($file.FullName) 
 }
 
-$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist"-Recurse
+$files = Get-ChildItem -Path "C:\HBSIS\Projetos\Dashboard_B2BG\dist" -file -Recurse
 foreach ($file in $files){
 	$oldIMG = 'img/'
 	$newIMG = 'html/DashboardB2BG/img/'
@@ -24,3 +24,19 @@ foreach ($file in $files){
 	#write replaced content back to the file
 	$content -replace $oldIMG,$newIMG | Out-File $($file.FullName) 
 }
+#>
+
+function replaceFiles
+{
+	Param($path,$replaceFrom,$replaceTo)
+	$files = Get-ChildItem -Path $path -file -Recurse
+	foreach ($file in $files){
+		$content = Get-Content $($file.FullName) -Raw
+		#write replaced content back to the file
+		$content -replace $replaceFrom,$replaceTo | Out-File $($file.FullName) 
+	}
+}
+
+replaceFiles "C:\HBSIS\Projetos\Dashboard_B2BG\dist" '/js/' '/html/DashboardB2BG/js/'
+replaceFiles "C:\HBSIS\Projetos\Dashboard_B2BG\dist" 'css/' 'html/DashboardB2BG/css/'
+replaceFiles "C:\HBSIS\Projetos\Dashboard_B2BG\dist" 'img/' 'html/DashboardB2BG/img/'
